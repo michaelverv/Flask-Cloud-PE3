410c8e698e05d736d56e29b2af1bebfd4774a7d8
michaelverv <r0844989@student.thomasmore.be>
2024-05-15 21:39:11 +0200

    pls make it stop

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 import os
+import pymysql
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = 'do-or-do-not-there-is-no-try'
-#    SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:testtest@database-1.crcicqci0hpe.us-east-1.rds.amazonaws.com:3306'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
