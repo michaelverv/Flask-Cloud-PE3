830f780655175c142389abe1a2fccf999616daae
Michael Vervoort <113921262+michaelverv@users.noreply.github.com>
2025-02-25 10:40:43 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = 'do-or-do-not-there-is-no-try'
     # SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:testtest@cloud-pe3-mv-database.crcicqci0hpe.us-east-1.rds.amazonaws.com:3306/Cloud'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://
     SQLALCHEMY_TRACK_MODIFICATIONS = False
